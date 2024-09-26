639e01b12e8cd97a80fa724e9d365cb1a5eb650e
whoisseth <utkarshseth2019@gmail.com>
2024-09-26 16:58:21 +0530

    add notification component

--- src/app/[route-name]/_components/add-hero-section.tsx
+++ src/app/[route-name]/_components/add-hero-section.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import React from "react";
 import { buttonVariants } from "@/components/ui/button";
 import { useCanEditPortfolio } from "@/hooks/useCanEditPortfolio";
 import { cn } from "@/lib/utils";
 import { User } from "lucia";
 import Link from "next/link";
-
+import Notification from "@/components/notification";
 
 type Props = {
   user: User | undefined;
@@ -14,16 +15,19 @@ type Props = {
 export default function AddHeroSection({ user }: Props) {
   const canEdit = useCanEditPortfolio(user);
   if (!canEdit) return null;
-  return (
-    <div className="mb-4 flex items-center justify-between">
-      <h2 className="text-2xl font-bold">Hero Section</h2>
 
-      <Link
-        className={cn(buttonVariants({ variant: "outline" }))}
-        href="/create-portfolio"
-      >
-        Add Hero Section
-      </Link>
-    </div>
+  return (
+    <>
+      <Notification message="Project section issue fixed and updated." />
+      <div className="mb-4 mt-1 flex items-center justify-between">
+        <h2 className="text-2xl font-bold">Hero Section</h2>
+        <Link
+          className={cn(buttonVariants({ variant: "outline" }))}
+          href="/create-portfolio"
+        >
+          Add Hero Section
+        </Link>
+      </div>
+    </>
   );
 }
---
+++ src/components/notification.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {
+  message: string;
+};
+
+export default function Notification({ message }: Props) {
+  return (
+    <p className="w-full bg-green-100 p-1.5 text-center text-xs font-semibold text-green-800 rounded-sm">
+      {message}
+    </p>
+  );
+}
