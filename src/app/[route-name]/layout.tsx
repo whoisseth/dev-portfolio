af6dcf3424a5ab805670b7007ca636566fc118f0
whoisseth <utkarshseth2019@gmail.com>
2024-10-01 22:55:58 +0530

    notificatoin added for project img

--- src/app/[route-name]/layout.tsx
+++ src/app/[route-name]/layout.tsx
@@ -2,6 +2,9 @@ import React, { ReactNode } from "react";
 import Navbar from "./_components/navbar";
 import { getCurrentUser } from "@/lib/session";
 import { getUserRoute } from "@/actions/create-portfolio-actions";
+import Notification from "@/components/notification";
+
+const SHOW_NEW_FEATURE_NOTIFICATION = true;
 
 export default async function Layout({
   children,
@@ -12,6 +15,9 @@ export default async function Layout({
   const userRoute = await getUserRoute(user?.id || null);
   return (
     <div>
+      {SHOW_NEW_FEATURE_NOTIFICATION && (
+        <Notification message="New feature: You can now upload images for your projects!" />
+      )}
       <Navbar user={user} userRoute={userRoute} />
       <div className="container mx-auto w-full px-4 sm:px-8">{children}</div>
     </div>
--- src/components/notification.tsx
+++ src/components/notification.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function Notification({ message }: Props) {
   return (
-    <p className="w-full bg-green-100 p-1.5 text-center text-xs font-semibold text-green-800 rounded-sm">
+    <p className="w-full bg-green-100 p-1.5 text-center text-xs font-semibold text-green-800 ">
       {message}
     </p>
   );
