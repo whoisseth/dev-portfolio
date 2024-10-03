192b5750481009b8f8a269526e44a73405a63a66
whoisseth <utkarshseth2019@gmail.com>
2024-10-04 01:16:16 +0530

    SEO added for route name

--- src/app/[route-name]/layout.tsx
+++ src/app/[route-name]/layout.tsx
@@ -3,8 +3,49 @@ import Navbar from "./_components/navbar";
 import { getCurrentUser } from "@/lib/session";
 import { getUserRoute } from "@/actions/create-portfolio-actions";
 import Notification from "@/components/notification";
+import { getHeroSectionData } from "@/actions/create-portfolio-actions";
+
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+// this is the reference for dynamic metadata
+// https://nextjs.org/docs/app/building-your-application/optimizing/metadata#dynamic-metadata
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const routeName = params.id;
+
+  // Fetch hero section data using the route name
+  const heroSection = await getHeroSectionData(routeName);
+
+  if (!heroSection) {
+    return {
+      title: "Default Title",
+      description: "Default Description",
+    };
+  }
+
+  return {
+    title: `${heroSection.hero_section.fullName} - ${heroSection.hero_section.title}`,
+    description: heroSection.hero_section.description,
+  };
+}
 
 const SHOW_NEW_FEATURE_NOTIFICATION = true;
+
+// export const metadata: Metadata = {
+//   title: "Portly Dev Portfolio Builder",
+//   icons: [
+//     { rel: "icon", type: "image/png", sizes: "48x48", url: "/favicon.ico" },
+//   ],
+//   keywords: "Portfolio",
+//   description:
+//     "Portly is Dev Portfolio Builder: A simple, customizable tool to create and showcase professional developer portfolios with ease.",
+// };
 
 export default async function Layout({
   children,
